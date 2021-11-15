962094482b31bf79a9e14c41e846a3e3b03934ac
dev-aman-singh <amansingh1031997@gmail.com>
2021-11-15 17:31:47 +0530

    updated one

--- src/com/renju/training/testng/SampleSeleniumTest.java
+++ src/com/renju/training/testng/SampleSeleniumTest.java
@@ -28,16 +28,21 @@ public class SampleSeleniumTest {
 	public void validateGoogleId() throws Exception {
 		System.out.println("Opening Browser");
 		driver.get("http://www.google.com");
+		//driver.findElement(By.xpath("//*[@id=\"gbw\"]/div/div/div[1]/div[1]/a")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Gmail')]")).click();
+		//a[contains(text(),'Gmail')]
 		System.out.println("Clicking Gmail Link");
-		driver.findElement(By.xpath("//*[@id=\"gbw\"]/div/div/div[1]/div[1]/a")).click();
 		System.out.println("Clicking Sign In link");
-		driver.findElement(By.xpath("/html/body/nav/div/a[2]")).click();
+		//driver.findElement(By.xpath("/html/body/nav/div/a[2]")).click();
+		//a[contains(text(),'Sign in')]
+		driver.findElement(By.xpath("//a[contains(text(),'Sign in')]")).click();
 		System.out.println("Entering username");
 		driver.findElement(By.xpath("//*[@id=\"identifierId\"]")).sendKeys("amansingh1031997");
 		System.out.println("Clicking Next button");
-		driver.findElement(By.xpath("//*[@id=\"identifierNext\"]/content/span")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Next')]")).click();
 		Thread.sleep(5000);
-		boolean textFound = driver.getPageSource().contains("Forgot password");
+		boolean textFound = driver.getPageSource().contains("Forgot");
+		System.out.println(textFound);
 		AssertJUnit.assertTrue(textFound);
 	}
 	
@@ -46,4 +51,3 @@ public class SampleSeleniumTest {
 		driver.quit();
 	}
 }
-
